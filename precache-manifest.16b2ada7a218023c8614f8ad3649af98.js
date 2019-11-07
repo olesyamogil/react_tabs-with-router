37deb3f8e1c70b949ce7d58f6df0bff4acee0303
olesyamogil <olesya.mogil@gmail.com>
2019-11-07 19:15:14 +0200

    Updates

---
+++ precache-manifest.16b2ada7a218023c8614f8ad3649af98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61020f69827f8ea0993470d68e874a12",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "aea12d80c59a68228b1d",
+    "url": "/react_tabs-with-router/static/css/main.5efc4f9c.chunk.css"
+  },
+  {
+    "revision": "f65ace08762c23fbd9da",
+    "url": "/react_tabs-with-router/static/js/2.10bdab4f.chunk.js"
+  },
+  {
+    "revision": "aea12d80c59a68228b1d",
+    "url": "/react_tabs-with-router/static/js/main.72ad464d.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
